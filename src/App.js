9bb624594bea6a4deb9757e4171afe4aac6695f3
Tale152 <alessandro.talmi@studio.unibo.it>
2021-11-30 22:33:26 +0100

    added treasure hunt logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { Row, Col } from 'react-bootstrap'
+import crown from './res/crown.png'
+
 function App() {
   return (
-    <p>Hello, world!</p>
+    <Row className="justify-content-center">
+        <Col md={2} className="text-center text-md-right">
+          <img src={crown} alt="Kings quest" width="256" height="256"/>
+        </Col>
+    </Row>
   )
 }
 
