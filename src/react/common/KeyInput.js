41a668b0801f90f041dd9ab88ff4c827c3212b63
Tale152 <alessandro.talmi@studio.unibo.it>
2021-11-30 23:57:04 +0100

    added onChange prop in KeyInput

--- src/react/common/KeyInput.js
+++ src/react/common/KeyInput.js
@@ -3,7 +3,12 @@ import Form from 'react-bootstrap/Form'
 export default function KeyInput(props){
     return (
         <Form.Group controlId={props.id} className="mx-2 my-4">
-            <Form.Control type="password" placeholder="Inserisci la chiave corretta" className="text-center"/>
+            <Form.Control
+                type="password"
+                placeholder="Inserisci la chiave corretta"
+                className="text-center"
+                onChange={e => props.onChange(e.target.value)}
+            />
         </Form.Group>
     )
 }
--- src/react/landingPage/LandingPage.js
+++ src/react/landingPage/LandingPage.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 
 import Header from '../common/Header'
@@ -6,19 +7,25 @@ import MyRow from '../common/MyRow'
 import ContinueButton from '../common/ContinueButton'
 
 export default function LandingPage(props){
+
+    const [firstKey, setFirstKey] = useState("")
+    const [secondKey, setSecondKey] = useState("")
+    const [thirdKey, setThirdKey] = useState("")
+    const [fourthKey, setFourthKey] = useState("")
+
     return (
         <div>
             <Header />
             <MyRow>
                 <Form>
-                    <KeyInput id={"form.firstKey"} />
-                    <KeyInput id={"form.secondKey"} />
-                    <KeyInput id={"form.thirdKey"} />
-                    <KeyInput id={"form.fourthKey"} />
+                    <KeyInput id={"form.firstKey"} onChange={setFirstKey}/>
+                    <KeyInput id={"form.secondKey"} onChange={setSecondKey}/>
+                    <KeyInput id={"form.thirdKey"} onChange={setThirdKey}/>
+                    <KeyInput id={"form.fourthKey"} onChange={setFourthKey}/>
                 </Form>
             </MyRow>
             <MyRow>
-                <ContinueButton onClick={() => console.log("click")} />
+                <ContinueButton onClick={() => console.log(firstKey)} />
             </MyRow>
         </div>
     )
