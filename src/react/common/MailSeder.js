5f97e848dcb63eac1a14706df32efbcc85364f37
Tale152 <alessandro.talmi@studio.unibo.it>
2021-12-04 17:54:47 +0100

    updated MailSender to cover env variables missing

--- src/react/common/MailSeder.js
+++ src/react/common/MailSeder.js
@@ -1,7 +1,8 @@
 import emailjs from 'emailjs-com'
 
 export default function sendMail(template){
-    if (process.env.REACT_APP_EMAILJS_USER_CODE !== null && template !== null){
+    const userCode = process.env.REACT_APP_EMAILJS_USER_CODE
+    if (userCode !== null && userCode !== undefined && userCode !== "" && template !== null && template !== undefined && template !== ""){
         emailjs.send('gmail', template, {}, process.env.REACT_APP_EMAILJS_USER_CODE)
     }
 }
